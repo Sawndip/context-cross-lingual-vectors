8017f634d4e65533bf8743a01885eb0d101203c3
Manaal Faruqui <manaalfr@gmail.com>
2014-10-20 14:17:51 -0400

    source word vector added to the maxed layer

--- conv.cc
+++ conv.cc
@@ -42,10 +42,12 @@ class Model {
   */
   AMat p1, p2, p3;
   unsigned k;
+  adouble c;
   /* Adadelta memory */
   Mat ad_p1, ad_g_p1, ad_p2, ad_g_p2, ad_p3, ad_g_p3;
+  double ad_c, ad_g_c;
   /* Word vectors */
-  unsigned window_size, src_len, tgt_len;
+  unsigned window_size, src_len, tgt_len, hidden_len;
   mapStrUnsigned src_vocab, tgt_vocab;
   vector<Col> src_word_vecs, tgt_word_vecs;
       
@@ -55,16 +57,18 @@ class Model {
     ReadVecsFromFile(tgt_vec_file, &tgt_vocab, &tgt_word_vecs);
     src_len = src_word_vecs[0].size();
     tgt_len = tgt_word_vecs[0].size();
-    k = kmax;  // select top-k after convolving
+    hidden_len = unsigned(0.25 * src_len);
     /* Params init */
     p1 = (0.6 / sqrt(filter_len * src_len)) * 
          AMat::Random(src_len, filter_len);
-    p2 = (0.6 / sqrt(k)) * AMat::Random(k, 1);
-    p3 = (0.6 / sqrt(tgt_len * src_len)) * AMat::Random(tgt_len, src_len);
+    p2 = (0.6 / sqrt(hidden_len * src_len)) * AMat::Random(hidden_len, src_len);
+    p3 = (0.6 / sqrt(tgt_len * hidden_len)) * AMat::Random(tgt_len, hidden_len);
+    c = 1;
     /* Adadelta init */
     ad_p1 = ad_g_p1 = Mat::Zero(src_len, filter_len);
-    ad_p2 = ad_g_p2 = Mat::Zero(kmax, 1);
-    ad_p3 = ad_g_p3 = Mat::Zero(tgt_len, src_len);
+    ad_p2 = ad_g_p2 = Mat::Zero(hidden_len, src_len);
+    ad_p3 = ad_g_p3 = Mat::Zero(tgt_len, hidden_len);
+    ad_c = ad_g_c = 0;
   }
 
   void convolve_narrow(const Mat& mat, const AMat& filter, AMat* res) {
@@ -94,13 +98,8 @@ class Model {
     convolve_narrow(new_sent, filter, res);
   }
 
-  void k_max(const AMat& mat, AMat* res) {
-    if (k == 1) {
-      (*res) = mat.rowwise().maxCoeff();
-    } else {
-      exit(0);
-      // Do this
-    }
+  void max(const AMat& mat, AMat* res) {
+    (*res) = mat.rowwise().maxCoeff();
   }
 
   adouble ComputePredError(const unsigned& src_word_id,
@@ -108,13 +107,16 @@ class Model {
     /* Make a sentence matrix by appending all word vectors
        Ignore all the noisy words (denoted by -1) */
     Col zero = Col::Zero(src_len), orig_col = sent_mat->col(src_word_id);
+    ACol azero = ACol::Zero(src_len);
     sent_mat->col(src_word_id) = zero;  // Set predict word to be zero
     /* Build the prediction model here */
-    AMat convolved, k_maxed;
+    AMat convolved, maxed;
     ACol non_lin;
     convolve_wide(*sent_mat, p1, &convolved);  // Wide convolution with filter
-    k_max(convolved, &k_maxed);  // k-max layer
-    non_lin = k_maxed * p2;  // Reduction to a vector
+    max(convolved, &maxed);  // k-max layer
+    /* Add source word vector to the maxed layer */
+    maxed = ElemwiseDiff(maxed, -1 * orig_col);
+    non_lin = p2 * maxed;  // Reduction to a vector of hidden_len
     ElemwiseTanh(&non_lin);  // Non-linearity
     ACol tgt_vec = p3 * non_lin;  // Predicted output
     /* Prediction done, replace the predict word column */
@@ -126,6 +128,7 @@ class Model {
     AdadeltaMatUpdate(RHO, EPSILON, &p1, &ad_p1, &ad_g_p1);
     AdadeltaMatUpdate(RHO, EPSILON, &p2, &ad_p2, &ad_g_p2);
     AdadeltaMatUpdate(RHO, EPSILON, &p3, &ad_p3, &ad_g_p3);
+    AdadeltaUpdate(RHO, EPSILON, &c, &ad_c, &ad_g_c);
   }
 
 };
@@ -200,6 +203,7 @@ void Train(const string& p_corpus, const string& a_corpus, const int& num_iter,
         cerr << num_words << "\r";
       }
       cerr << "\nError per word: "<< total_error/num_words << "\n";
+      cerr << model->c << endl;
       p_file.close();
       a_file.close();
     } else {
@@ -216,7 +220,7 @@ int main(int argc, char **argv){
          << " update_every " << " num_iter " << " outfilename\n";
     cerr << "Recommended: " << argv[0] << " parallel_corpus " 
          << " alignment_corpus " << " src_vec_corpus " << " tgt_vec_corpus "
-         << " 5 1 100 1" << " out.txt\n";
+         << " 5 1 50 2" << " out.txt\n";
     exit(0);
   }
   
@@ -236,9 +240,11 @@ int main(int argc, char **argv){
   cerr << "Model parameters" << endl;
   cerr << "----------------" << endl;
   cerr << "Input vector length: " << model.src_len << endl;
+  cerr << "Hidden vector length: " << model.hidden_len << endl;
   cerr << "Output vector length: " << model.tgt_len << endl;
   cerr << "----------------" << endl;
 
   Train(parallel_corpus, align_corpus, num_iter, update_every, &model, &s);
+  WriteParamsToFile(outfilename, model.p1, model.p2, model.p3);
   return 1;
 }
